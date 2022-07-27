f17cf8bd0ab8f68909ab18b5db244f1f990b05b8
NILGIRI\Soukenbicha <e18240@yonago.kosen-ac.jp>
2022-07-27 11:51:23 +0900

    2022/07/27 ゾンビの追尾、攻撃アニメーションを追加 窓をたたくゾンビの作業中

--- Pcontest2022/Assets/Scripts/EnemyMove.cs
+++ Pcontest2022/Assets/Scripts/EnemyMove.cs
@@ -6,23 +6,73 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMove : MonoBehaviour
 {
+    private Animator anim;
+
     [SerializeField]
     [Tooltip("追いかける対象")]
     private GameObject player;
 
-    private NavMeshAgent navMeshAgent;
+    private NavMeshAgent agent;
+
+    int attackInterval = 1;
+    bool attacking = false;
+    public bool moveEnabled = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = gameObject.GetComponent<Animator>();
+
         // NavMeshAgentを保持しておく
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void OnCollisionStay()
+    void Update()
     {
+        if (moveEnabled)
+        {
+            Move();
+        }
+        else
+        {
+            Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            StartCoroutine(AttackTimer());
+        }
+    }
+
+    IEnumerator AttackTimer()
+    {
+        if (!attacking)
+        {
+            attacking = true;
+            moveEnabled = false;
+            anim.SetTrigger("Attack");
+            yield return new WaitForSeconds(attackInterval);
+            attacking = false;
+            moveEnabled = true;
+        }
+        yield return null;
+    }
+
+    void Move()
+    {
+        anim.SetFloat("Speed", agent.speed, 0.1f, Time.deltaTime);
+
         // プレイヤーを目指して進む
-        navMeshAgent.destination = player.transform.position;
+        agent.destination = player.transform.position;
+    }
+
+    void Stop()
+    {
+        agent.speed = 0;
+        anim.SetFloat("Speed", agent.speed, 0.1f, Time.deltaTime);
     }
 }
