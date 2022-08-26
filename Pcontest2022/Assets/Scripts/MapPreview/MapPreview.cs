d7b24fc1dca12e39765ce490c1f9db58bdbf0e3f
NILGIRI\Soukenbicha <e18240@yonago.kosen-ac.jp>
2022-08-26 11:21:35 +0900

    8/26 Editor拡張でマップ作成を実装する方向に切り替え　データはJSON形式で管理

---
+++ Pcontest2022/Assets/Scripts/MapPreview/MapPreview.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPreview : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadCSV()
+    {
+        //TextAsset csvfile = Resources.Load(filePass) as TextAsset;
+        //Debug.Log(csvfile.text);
+
+    }
+}
