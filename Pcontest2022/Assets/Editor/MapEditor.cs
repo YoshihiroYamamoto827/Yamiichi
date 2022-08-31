584a2d6f501835ed06a9e16c19dd090c36932f8b
YAMAZEN-DESKTOP <e18240@yonago.kosen-ac.jp>
2022-09-01 00:00:18 +0900

    8/31 マップ作成システムの実装 出力はtxtのまま

--- Pcontest2022/Assets/Editor/MapEditor.cs
+++ Pcontest2022/Assets/Editor/MapEditor.cs
@@ -188,7 +188,7 @@ public class MapEditorSubWindow : EditorWindow
     //マップデータ
     private string[,] map;
     //グリッドの四角
-    private Rect[,] GetRect;
+    private Rect[,] gridRect;
     //親ウィンドウの参照
     private MapEditor parent;
 
@@ -206,6 +206,199 @@ public class MapEditorSubWindow : EditorWindow
     private void SetParent(MapEditor _parent)
     {
         parent = _parent;
+    }
+
+    //サブウィンドウの初期化
+    public void init()
+    {
+        mapSize = parent.MapSize;
+        gridSize = parent.GridSize;
+
+        //マップデータを初期化
+        map = new string[mapSize, mapSize];
+        for(int i = 0; i < mapSize; i++)
+        {
+            for(int j = 0; j < mapSize; j++)
+            {
+                map[i, j] = "";
+            }
+        }
+        //グリッドデータを生成
+        gridRect = CreateGrid(mapSize);
+    }
+
+    void OnGUI()
+    {
+        //グリッド線を描画する
+        for(int yy = 0; yy < mapSize; yy++)
+        {
+            for(int xx = 0; xx < mapSize; xx++)
+            {
+                DrawGridLine(gridRect[yy, xx]);
+            }
+        }
+
+        //クリックされた位置を探してその場所に画像データを入れる
+        Event e = Event.current;
+        if(e.type == EventType.MouseDown)
+        {
+            Vector2 pos = Event.current.mousePosition;
+            int xx;
+
+            //x位置を探す
+            for (xx = 0; xx < mapSize; xx++)
+            {
+                Rect r = gridRect[0, xx];
+                if (r.x <= pos.x && pos.x <= r.x + r.width)
+                {
+                    break;
+                }
+            }
+
+            //y位置を探す
+            for(int yy = 0; yy < mapSize; yy++)
+            {
+                if (gridRect[yy, xx].Contains(pos))
+                {
+                    //消しゴムのときはデータを消す
+                    if (parent.SelectedImagePath.IndexOf("000") > -1)
+                    {
+                        map[yy, xx] = "";
+                    }
+                    else
+                    {
+                        map[yy, xx] = parent.SelectedImagePath;
+                    }
+                    Repaint();
+                    break;
+                }
+            }
+        }
+
+        //選択した画像を描画する
+        for(int yy = 0; yy < mapSize; yy++)
+        {
+            for(int xx = 0; xx < mapSize; xx++)
+            {
+                if (map[yy, xx] != null && map[yy, xx].Length > 0)
+                {
+                    Texture2D tex = (Texture2D)AssetDatabase.LoadAssetAtPath(map[yy, xx], typeof(Texture2D));
+                    GUI.DrawTexture(gridRect[yy, xx], tex);
+                }
+            }
+        }
+
+        //出力ボタン
+        Rect rect = new Rect(0, WINDOW_H - 50, 300, 50);
+        GUILayout.BeginArea(rect);
+        if(GUILayout.Button("output file", GUILayout.MinWidth(300), GUILayout.MinHeight(50)))
+        {
+            OutputFile();
+        }
+        GUILayout.FlexibleSpace();
+        GUILayout.EndArea();
+    }
+
+    //グリッドデータを作成
+    private Rect[,] CreateGrid(int div)
+    {
+        int sizeW = div;
+        int sizeH = div;
+
+        float x = 0.0f;
+        float y = 0.0f;
+        float w = gridSize;
+        float h = gridSize;
+
+        Rect[,] resultRects = new Rect[sizeH, sizeW];
+
+        for(int yy = 0; yy < sizeH; yy++)
+        {
+            x = 0.0f;
+            for(int xx = 0; xx < sizeW; xx++)
+            {
+                Rect r = new Rect(new Vector2(x, y), new Vector2(w, h));
+                resultRects[yy, xx] = r;
+                x += w;
+            }
+            y += h;
+        }
+        return resultRects;
+    }
+
+    //グリッド線を描画
+    private void DrawGridLine(Rect r)
+    {
+        //grid
+        Handles.color = new Color(1f, 1f, 1f, 0.5f);
+
+        //upper line
+        Handles.DrawLine(
+            new Vector2(r.position.x, r.position.y),
+            new Vector2(r.position.x + r.size.x, r.position.y));
+
+        //bottom line
+        Handles.DrawLine(
+            new Vector2(r.position.x, r.position.y + r.size.y),
+            new Vector2(r.position.x + r.size.x, r.position.y + r.size.y));
+
+        //left line
+        Handles.DrawLine(
+            new Vector2(r.position.x, r.position.y),
+            new Vector2(r.position.x + r.size.x, r.position.y + r.size.y));
+
+        //right line
+        Handles.DrawLine(
+            new Vector2(r.position.x + r.size.x, r.position.y),
+            new Vector2(r.position.x + r.size.x, r.position.y + r.size.y));
+    }
+
+    //ファイルで出力
+    private void OutputFile()
+    {
+        string path = parent.OutputFilePath();
+
+        FileInfo fileInfo = new FileInfo(path);
+        StreamWriter sw = fileInfo.AppendText();
+        sw.WriteLine(GetMapStrFormat());
+        sw.Flush();
+        sw.Close();
+
+        //完了ポップアップ
+        EditorUtility.DisplayDialog("MapEditor", "output file success\n" + path, "ok");
+    }
+
+    //出力するマップデータ整形
+    private string GetMapStrFormat()
+    {
+        string result = "";
+        for(int i = 0; i < mapSize; i++)
+        {
+            for(int j = 0; j < mapSize; j++)
+            {
+                result += OutputDataFormat(map[i, j]);
+                if (j < mapSize - 1)
+                {
+                    result += ",";
+                }
+            }
+            result += "\n";
+        }
+        return result;
+    }
+
+    private string OutputDataFormat(string data)
+    {
+        if(data != null && data.Length > 0)
+        {
+            string[] tmps = data.Split('/');
+            string fileName = tmps[tmps.Length - 1];
+            return fileName.Split('/')[0];
+        }
+        else
+        {
+            return "";
+        }
     }
 }
 
