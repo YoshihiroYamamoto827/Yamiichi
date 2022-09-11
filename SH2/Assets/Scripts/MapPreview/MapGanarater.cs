f9e5096ea11fcebf2f7deca7d3d273acd63d2bed
YAMAZEN-DESKTOP <e18240@yonago.kosen-ac.jp>
2022-09-11 22:52:50 +0900

    9/11 Jsonファイルからマップのオブジェクト生成をするプログラムを実装 マップサイズや変数指定など細かい調整は必要

--- SH2/Assets/Scripts/MapPreview/MapGanarater.cs
+++ SH2/Assets/Scripts/MapPreview/MapGanarater.cs
@@ -22,7 +22,6 @@ public class MapGanarater : MonoBehaviour
         public string objectname;
     }
 
-    
     void Start()
     {
         Jsondata jsondata = new Jsondata();
@@ -33,16 +32,50 @@ public class MapGanarater : MonoBehaviour
 
     public void OnPreviewButton()
     {
-        var assetBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/samplejson4");
-        string inputString = assetBundle.LoadAsset<TextAsset>("Assets/MapData/SampleJson4.json").ToString();
+        //JsonファイルのAssetBundleとAssetBundle内の対応するJsonファイルの読み込み
+        var JsonassetBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/samplejson5");
+        string inputString = JsonassetBundle.LoadAsset<TextAsset>("Assets/MapData/SampleJson5.json").ToString();
 
-
-        JsonFile = inputFieldManager.SendJsonFile();
-        ImgDir = inputFieldManager.SendImgDirectory();
-
-        //string inputString = MapData.Load<TextAsset>("SampleJson4");
-
+        //Jsonファイルのデータをクラス変数に代入
         Jsondata inputjson = JsonUtility.FromJson<Jsondata>(inputString);
-        Debug.Log(inputjson.mapdata[1]);
+
+        Debug.Log(inputjson.mapdata[0].xcoor);
+
+        //オブジェクトのprefabが入っているフォルダのAssetBundleの読み込み、AssetBundle内から各オブジェクトの変数に代入
+        var ObjectAssetBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/objects");
+        var Wall = ObjectAssetBundle.LoadAsset<GameObject>("Assets/Resources/Objects/Wall.prefab");
+        var Door = ObjectAssetBundle.LoadAsset<GameObject>("Assets/Resources/Objects/Door.prefab");
+        var Floor = ObjectAssetBundle.LoadAsset<GameObject>("Assets/Resources/Objects/Floor.prefab");
+        var Item = ObjectAssetBundle.LoadAsset<GameObject>("Assets/Resources/Objects/Item.prefab");
+        var Player = ObjectAssetBundle.LoadAsset<GameObject>("Assets/Resources/Objects/Player.prefab");
+
+        for(int i = 0; i < 100; i++)
+        {
+            Instantiate(Floor, new Vector3(inputjson.mapdata[i].xcoor, 0f, inputjson.mapdata[i].ycoor),Quaternion.identity);
+
+            if(inputjson.mapdata[i].objectname != null)
+            {
+                switch(inputjson.mapdata[i].objectname){
+                    case "Capture\\001.png":
+                        Instantiate(Wall, new Vector3(inputjson.mapdata[i].xcoor, 2f, inputjson.mapdata[i].ycoor), Quaternion.identity);
+                        break;
+                    case "Capture\\002.png":
+                        Instantiate(Door, new Vector3(inputjson.mapdata[i].xcoor, 1.5f, inputjson.mapdata[i].ycoor), Quaternion.identity);
+                        break;
+                    //case "Capture\\003.png":
+                    //Instantiate(Wall, new Vector3(inputjson.mapdata[i].xcoor, 1f, inputjson.mapdata[i].ycoor), Quaternion.identity);
+                    //break;
+                    //case "Capture\\004.png":
+                    //Instantiate(Wall, new Vector3(inputjson.mapdata[i].xcoor, 1f, inputjson.mapdata[i].ycoor), Quaternion.identity);
+                    //break;
+                    case "Capture\\005.png":
+                        Instantiate(Item, new Vector3(inputjson.mapdata[i].xcoor, 1.5f, inputjson.mapdata[i].ycoor), Quaternion.identity);
+                        break;
+                    case "Capture\\006.png":
+                        Instantiate(Player, new Vector3(inputjson.mapdata[i].xcoor, 1.5f, inputjson.mapdata[i].ycoor), Quaternion.identity);
+                        break;
+                }
+            }
+        }
     }
 }
