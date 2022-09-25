273133e34a120f67352ef223076c83ebcebcdf5d
YAMAZEN-DESKTOP <e18240@yonago.kosen-ac.jp>
2022-09-25 16:49:32 +0900

    9/25

--- SH2/Assets/Scripts/SendMapFolderName.cs
+++ SH2/Assets/Scripts/SendMapFolderName.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 public class SendMapFolderName : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath);
-        FileInfo[] info = dir.GetFiles("");
+        Dropdown MCDropdown;
+        List<string> MapList = new List<string>();
+
+        DirectoryInfo dir = new DirectoryInfo(Application.streamingAssetsPath + "/Map");
+        FileInfo[] info = dir.GetFiles("*.");
         foreach (FileInfo f in info)
         {
             Debug.Log(f.Name);
+
+            if(f.Name!="map")MapList.Add(f.Name);
         }
+
+        //ドロップダウンのコンポーネント取得と表示するリストの宣言
+        MCDropdown = GameObject.Find("MapChooseDropdown").GetComponent<Dropdown>();
+
+        //ドロップダウンのOptionsをクリア
+        MCDropdown.ClearOptions();
+
+        //リストをドロップダウンに追加
+        MCDropdown.AddOptions(MapList);
     }
 
     // Update is called once per frame
