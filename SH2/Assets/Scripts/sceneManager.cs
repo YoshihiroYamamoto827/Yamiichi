4741a3280d67fdbcb9d2a26347b72b69239a391e
I5-12600K <e18225@yonago.kosen-ac.jp>
2022-09-25 22:05:54 +0900

    9/25　西村

--- SH2/Assets/Scripts/sceneManager.cs
+++ SH2/Assets/Scripts/sceneManager.cs
@@ -2,14 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class sceneManager : MonoBehaviour
 {
     public GameObject tutorialPanel;
+    public GameObject HelpPanel;
+    public GameObject FadePanel;
+
+    float alpha;
+    public float alphaSpeed;
+    Image fadeAlpha;
+
+    private bool fadeout = false;
+    private bool fadein = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        fadeAlpha = FadePanel.GetComponent<Image>();
+        alpha = fadeAlpha.color.a;
+        fadein = true;
+    }
 
+    private void Update()
+    {
+        if (fadeout == true)
+            FadeOut();
+        if (fadein == true)
+            FadeIn();
     }
 
     public void StarttoGame()
@@ -41,4 +63,38 @@ public class sceneManager : MonoBehaviour
     {
         SceneManager.LoadScene("MapPreviewScene");
     }
+
+    public void HelpStart()
+    {
+        HelpPanel.SetActive(true);
+        Invoke("MapPreview", 6);
+        Invoke("FadeOut", 5);
+    }
+
+    //フェードイン・フェードアウト
+    #region
+    void FadeIn()
+    {
+        fadeAlpha.enabled = true;
+        alpha -= alphaSpeed;
+        fadeAlpha.color = new Color(0, 0, 0, alpha);
+        if (alpha <= 0)
+        {
+            fadein = false;
+            fadeAlpha.enabled = false;
+        }
+    }
+
+    void FadeOut()
+    {
+        fadeout = true;
+        fadeAlpha.enabled = true;
+        alpha += alphaSpeed;
+        fadeAlpha.color = new Color(0, 0, 0, alpha);
+        if(alpha>=1)
+        {
+            fadeout = false;
+        }
+    }
+    #endregion
 }
