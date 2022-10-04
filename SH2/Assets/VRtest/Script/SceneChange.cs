8ec38873ada9a999be067d6e849ae28f502d543e
e18225 <e18225@yonago.kosen-ac.jp>
2022-10-04 16:09:17 +0900

    10/4 プレイヤーだいたい完成

--- SH2/Assets/VRtest/Script/SceneChange.cs
+++ SH2/Assets/VRtest/Script/SceneChange.cs
@@ -14,6 +14,8 @@ public class SceneChange : MonoBehaviour
     public GameObject Pointer;
     public GameObject Button;
     public Text ButtonText;
+    public GameObject PlayerController;
+    public GameObject FlashLight;
 
     private void Start()
     {
@@ -23,64 +25,72 @@ public class SceneChange : MonoBehaviour
         HelpPanel.SetActive(false);
         MapSelectPanel.SetActive(false);
         EndPanel.SetActive(false);
+        Button.SetActive(false);
+        Pointer.SetActive(false);
+        PlayerController.GetComponent<OVRPlayerController>().enabled = true;
+
+        if (SceneManager.GetActiveScene().name != "Game")
+        {
+            Button.SetActive(true);
+            Pointer.SetActive(true);
+            PlayerController.GetComponent<OVRPlayerController>().enabled = false;
+        }
 
         if (SceneManager.GetActiveScene().name == "Title")
         {
             TitlePanel.SetActive(true);
             ButtonText.text = "Next";
         }
-        if (SceneManager.GetActiveScene().name == "Help")
+        else if (SceneManager.GetActiveScene().name == "Help")
         {
             HelpPanel.SetActive(true);
             ButtonText.text = "GameStart";
         }
-        if (SceneManager.GetActiveScene().name == "MapSelect")
+        else if (SceneManager.GetActiveScene().name == "MapSelect")
         {
             MapSelectPanel.SetActive(true);
             ButtonText.text = "Title";
         }
-        if (SceneManager.GetActiveScene().name == "End")
+        else if (SceneManager.GetActiveScene().name == "End")
         {
             EndPanel.SetActive(true);
             ButtonText.text = "Title";
         }
-        if (SceneManager.GetActiveScene().name != "Game")
+        else if (SceneManager.GetActiveScene().name == "Game")
         {
-            Button.SetActive(true);
-            Pointer.SetActive(true);
+            GameObject obj = Instantiate(FlashLight, transform.position, Quaternion.identity);
         }
 
-        /*else if (SceneManager.GetActiveScene().name == "Help")
-            HelpPanel.SetActive(true);
-        else if (SceneManager.GetActiveScene().name == "MapSelect")
-            HelpPanel.SetActive(true);
-        else if (SceneManager.GetActiveScene().name == "End")
-            EndPanel.SetActive(true);*/
     }
 
     private void Update()
     {
         //マップ選択・タイトルへ割り込み
-        if(OVRInput.Get(OVRInput.RawButton.A)&&
-           OVRInput.Get(OVRInput.RawButton.B)&&
-           OVRInput.Get(OVRInput.RawButton.RIndexTrigger)&&
-           OVRInput.Get(OVRInput.RawButton.RHandTrigger)&&
+        if (OVRInput.Get(OVRInput.RawButton.A) &&
+           OVRInput.Get(OVRInput.RawButton.B) &&
+           OVRInput.Get(OVRInput.RawButton.RIndexTrigger) &&
+           OVRInput.Get(OVRInput.RawButton.RHandTrigger) &&
            OVRInput.Get(OVRInput.RawButton.Start))
         {
             if (SceneManager.GetActiveScene().name == "Title")
                 SceneManager.LoadScene("MapSelect");
-            else
+            else if (SceneManager.GetActiveScene().name != "Title" && SceneManager.GetActiveScene().name != "MapSelect")
+            {
                 SceneManager.LoadScene("Title");
+            }
         }
+
+        if (OVRInput.Get(OVRInput.RawButton.Y))
+            SceneManager.LoadScene("End");
     }
 
     public void FadeInvoke()
     {
-        if (SceneManager.GetActiveScene().name=="Help")
+        if (SceneManager.GetActiveScene().name == "Help")
             Invoke("GameScene", 3);
-        else if(SceneManager.GetActiveScene().name=="Title")
+        else if (SceneManager.GetActiveScene().name == "Title")
             Invoke("HelpScene", 3);
-        else if (SceneManager.GetActiveScene().name == "End")
+        else if (SceneManager.GetActiveScene().name == "End" || SceneManager.GetActiveScene().name == "MapSelect")
             Invoke("TitleScene", 3);
     }
 
