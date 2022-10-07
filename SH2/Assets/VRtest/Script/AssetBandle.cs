4b40bbcbf577a2602455dec0070c0b5db6041ee3
e18225 <e18225@yonago.kosen-ac.jp>
2022-10-07 11:28:09 +0900

    プレイヤー生成できた

--- SH2/Assets/VRtest/Script/AssetBandle.cs
+++ SH2/Assets/VRtest/Script/AssetBandle.cs
@@ -11,10 +11,11 @@ public class AssetBandle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Player = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/ovrplayer 2");
+        Player = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/OVRPlayer");
         player = Player.LoadAsset<GameObject>("Assets/VRtest/Scenes/OVRPlayer.prefab");
         Debug.Log(player.name);
         player = Instantiate(player, new Vector3(0, 1, 0), Quaternion.identity);
+        player.name = Player.name;
     }
 
     // Update is called once per frame
